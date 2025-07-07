afb94a123b751c49c3dbf6a064fd0f09e640e6df
Liming zhiyi <137495648+2642253902@users.noreply.github.com>
2025-07-07 21:18:55 +0800

    Add files via upload

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -2,5 +2,38 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import HomePage from "@/pages/HomePage.vue"
+import AboutPage from "@/pages/AboutPage.vue"
+import NewPage from "@/pages/NewpPage.vue"
+
+//1.配置路由规则
+const routes = [
+    { path: "/", redirect: "/home" },
+    { path: "/home", component: HomePage },
+    { path: "/about", component: AboutPage },
+    { path: "/new", component: NewPage, name: 'new' }
+]
+
+
+//2.创建路由器
+
+const router = createRouter({
+    history: createWebHashHistory(),//路由工作模式
+    routes
+})
+
+
+//3.加载路由器
+const app = createApp(App)
+app.use(router)
+
+import { createPinia } from 'pinia'
+
+
+const pinia = createPinia()
+
+app.use(pinia)
+
+app.mount('#app')
