4c3c9d4c404a0b355240049071041a85b55d818e
Liming zhiyi <137495648+2642253902@users.noreply.github.com>
2025-07-08 17:24:58 +0800

    Add files via upload

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -8,19 +8,36 @@ import HomePage from "@/pages/HomePage.vue"
 import AboutPage from "@/pages/AboutPage.vue"
 import NewPage from "@/pages/NewpPage.vue"
 
+import NewsDetail1 from "@/pages/NewsDetail1.vue"
+import NewsDetail2 from "@/pages/NewsDetail2.vue"
+
 //1.配置路由规则
 const routes = [
     { path: "/", redirect: "/home" },
     { path: "/home", component: HomePage },
     { path: "/about", component: AboutPage },
-    { path: "/new", component: NewPage, name: 'new' }
+    {
+        path: "/new", component: NewPage, name: 'new',
+        children: [
+            {
+                path: "1",
+                component: NewsDetail1,
+
+            },
+            {
+                name: "news2",
+                path: "2/:id/:title/:content", //new/2/1/标题/内容
+                component: NewsDetail2,
+            }
+        ]
+    }
 ]
 
 
 //2.创建路由器
 
 const router = createRouter({
-    history: createWebHashHistory(),//路由工作模式
+    history: createWebHistory(),//路由工作模式
     routes
 })
 
@@ -30,10 +47,12 @@ const app = createApp(App)
 app.use(router)
 
 import { createPinia } from 'pinia'
-
-
 const pinia = createPinia()
-
 app.use(pinia)
+
+import ELementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+const ElementPlus = ELementPlus
+app.use(ElementPlus)
 
 app.mount('#app')
---
+++ vue-project/src/pages/NewsDetail1.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>新闻ID:{{ query.id }}</p>
+    <p>新闻ID:{{ query.title }}</p>
+    <p>新闻ID:{{ query.content }}</p>
+</template>
+<script lang="ts" setup>
+import { toRef } from 'vue';
+import { useRoute } from 'vue-router';
+
+let route = useRoute();
+console.log(route.query);
+
+let query = toRef(route, 'query');
+
+</script>
+<style scoped></style>
